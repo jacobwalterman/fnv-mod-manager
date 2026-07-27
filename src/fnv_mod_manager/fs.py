7149990e57e36d4561e514ec8ec6c040b9816373
Jacob Walterman <jacob.walterman.dev@gmail.com>
2026-07-27 09:50:28 -0500

    roll install_mods.py into fs.py

--- src/fnv_mod_manager/fs.py
+++ src/fnv_mod_manager/fs.py
@@ -1,7 +1,17 @@
 import os
 from pathlib import Path 
+import subprocess
 
 PACKAGE_NAME = "fnv-mod-manager"
+
+def extract_archive(archive_path, dest_dir):
+    subprocess.run(
+        ["7z", "x", str(archive_path), f"-o{dest_dir}", "-y"],
+        check=True,
+    )
+
+def extract_archive_to_mods_folder(archive_path):
+    extract_archive(archive_path, get_mod_manager_mods_folder())
 
 def data_dir() -> Path:
     base = os.environ.get("XDG_DATA_HOME")
@@ -9,6 +19,9 @@ def data_dir() -> Path:
 
 def get_mod_manager_data_folder():
     return data_dir() / PACKAGE_NAME
+
+def get_mod_manager_mods_folder():
+    return get_mod_manager_data_folder() / "mods"
 
 ####### DEFAULT FOLDER STRUCTURE
 root_folder = Path("Fallout New Vegas")
